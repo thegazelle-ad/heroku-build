fdd54538d8a6a698ba7a21d5723b71212da7df53
lingz <lz781@nyu.edu>
2013-08-10 23:17:14 +1200

    syntax fix

--- config/public/wp-content/themes/gridlock/searchform.php
+++
@@ -1,3 +0,0 @@
-<form class="navbar-form" action="<?php echo home_url( '/' ); ?>" method="get" role="search" id="searchForm">
-  <input type="text" class="form-control col-lg-8" name="s" id="s" placeholder="Search">
-</form>
